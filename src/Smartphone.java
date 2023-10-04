3ba4ea1a35b335bfa1eeb5fc27b38ddf67eedfe6
kyrgyzbaeva <133115394+kyrgyzbaeva@users.noreply.github.com>
2023-10-04 11:45:37 +0600

    Smartphone.java

--- src/Smartphone.java
+++ src/Smartphone.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Smartphone {
+public class Smartphone extends Device {
+    private double screenSize;
+    private int cameraResolution;
+
+
+    public Smartphone(String type, double price, double weight, double screenSize, int cameraResolution) {
+        super(type, price, weight);
+        this.screenSize = screenSize;
+        this.cameraResolution = cameraResolution;
+    }
+
+    public double getScreenSize() {
+        return screenSize;
+    }
+
+    public void setScreenSize(double screenSize) {
+        this.screenSize = screenSize;
+    }
+
+    public int getCameraResolution() {
+        return cameraResolution;
+    }
+
+    public void setCameraResolution(int cameraResolution) {
+        this.cameraResolution = cameraResolution;
+    }
+
 }
+
+
+
+
