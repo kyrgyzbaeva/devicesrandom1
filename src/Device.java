a1493f5e04ecdfabd1181d0ef11900f3d9daf7fd
kyrgyzbaeva <133115394+kyrgyzbaeva@users.noreply.github.com>
2023-10-04 11:40:52 +0600

    Device.java

--- src/Device.java
+++ src/Device.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class device {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Step 1: Ask the user to input the number of devices
+        System.out.print("Enter the number of devices you want to create (1-20): ");
+        int numDevices = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
+        if (numDevices < 1 || numDevices > 20) {
+            System.out.println("Please enter a valid number between 1 and 20.");
+            return;
+        }
+
+        // Step 2: Generate random instances of the derived classes
+        ArrayList<Device> devices = new ArrayList<>();
+        Random random = new Random();
+        String[] deviceTypes = { "Smartphone", "Laptop", "Tablet", "Desktop", "Smartwatch" };
+
+        for (int i = 0; i < numDevices; i++) {
+            String randomType = deviceTypes[random.nextInt(deviceTypes.length)];
+            double randomPrice = 100 + random.nextDouble() * 1900; // Random price between $100 and $2000
+            double randomWeight = 200 + random.nextDouble() * 1800; // Random weight between 200g and 2000g
+
+            Device device = new Device(randomType, randomPrice, randomWeight);
+            devices.add(device);
+        }
+
+        // Step 3: Calculate and display information
+        Set<String> distinctTypes = new HashSet<>();
+        double totalPrice = 0;
+        double totalWeight = 0;
+
+        for (Device device : devices) {
+            distinctTypes.add(device.getType());
+            totalPrice += device.getPrice();
+            totalWeight += device.getWeight();
+        }
+
+        System.out.println("\nInformation:");
+        System.out.println("Number of distinct device types created: " + distinctTypes.size());
+        System.out.println("Total price of all devices: $" + totalPrice);
+        System.out.println("Total weight of all devices: " + totalWeight + " grams");
+
+        scanner.close();
+    }
 }
+
