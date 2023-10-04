f3dc567f9723e068885b7427d91560b4cadfe10f
kyrgyzbaeva <133115394+kyrgyzbaeva@users.noreply.github.com>
2023-10-04 11:46:17 +0600

    Tablet.java

--- src/Tablet.java
+++ src/Tablet.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Tablet {
+public class Tablet extends Device {
+    private String operatingSystem;
+    private boolean hasStylus;
+
+    public Tablet(String type, double price, double weight, String operatingSystem, boolean hasStylus) {
+        super(type, price, weight);
+        this.operatingSystem = operatingSystem;
+        this.hasStylus = hasStylus;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+    public boolean hasStylus() {
+        return hasStylus;
+    }
+
+    public void setHasStylus(boolean hasStylus) {
+        this.hasStylus = hasStylus;
+    }
+
+    // You can add other methods specific to tablets here
 }
+
+
+
