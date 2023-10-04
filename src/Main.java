6cda3da9ee2a0636e655fedd50f5df4a1d5d593f
kyrgyzbaeva <133115394+kyrgyzbaeva@users.noreply.github.com>
2023-10-04 11:45:11 +0600

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,53 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main{
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.print("Enter the number of devices you want to create (1-20): ");
+        int numDevices = scanner.nextInt();
+        scanner.nextLine(); 
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        if (numDevices < 1 || numDevices > 20) {
+            System.out.println("Please enter a valid number between 1 and 20.");
+            return;
         }
+
+        
+        ArrayList<Device> devices = new ArrayList<>();
+        Random random = new Random();
+        String[] deviceTypes = { "Smartphone", "Laptop", "Tablet", "Desktop", "Smartwatch" };
+
+        for (int i = 0; i < numDevices; i++) {
+            String randomType = deviceTypes[random.nextInt(deviceTypes.length)];
+            double randomPrice = 100 + random.nextDouble() * 1900; 
+            double randomWeight = 200 + random.nextDouble() * 1800; 
+
+            Device device = new Device(randomType, randomPrice, randomWeight);
+            devices.add(device);
+        }
+
+        
+        Set<String> distinctTypes = new HashSet<>();
+        double totalPrice = 0;
+        double totalWeight = 0;
+
+        for (Device device : devices) {
+            distinctTypes.add(device.getType());
+            totalPrice += device.getPrice();
+            totalWeight += device.getWeight();
+        }
+
+        System.out.println("\nInformation:");
+        System.out.println("Number of distinct device types created: " + distinctTypes.size());
+        System.out.println("Total price of all devices: $" + totalPrice);
+        System.out.println("Total weight of all devices: " + totalWeight + " grams");
+
+        scanner.close();
     }
 }
+`
